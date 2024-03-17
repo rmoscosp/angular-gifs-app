92db98cd3408a5823a6ee47abf18c6239e4f02b4
Robinson Moscoso <robinsonm@mill5.com>
2024-03-17 05:43:40 -0500

    antes de guardar

--- src/app/gifs/components/card-list/card-list.component.ts
+++ src/app/gifs/components/card-list/card-list.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+
+import { Gif } from './../../interfaces/gifs.interfaces';
 
 @Component({
   selector: 'gifs-card-list',
@@ -6,5 +8,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./card-list.component.css']
 })
 export class CardListComponent {
+
+  @Input()
+  public gifs : Gif[] = [];
 
 }
